e3437baab30b9925899a25f52596831be7f76294
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2019-01-26 18:41:18 -0600

    first model

--- bin/rename.py
+++ bin/rename.py
@@ -1,22 +1,34 @@
 from os import rename, listdir, chdir
+from shutil import move
 
-def rename_files():
+def rename_files(directory, filetype):
+
+    chdir('../data/' + directory)
+    for filename in listdir('.'):
+        
+        if filename.endswith('_UL' + filetype):
+            rename(filename, '0_' + filename.split('_')[0] + filetype)
+
+        elif filename.endswith('_LL' + filetype):
+            rename(filename, '1_' + filename.split('_')[0] + filetype)
+
+        elif filename.endswith('_UR' + filetype):
+            rename(filename, '2_' + filename.split('_')[0] + filetype)
+
+        elif filename.endswith('_LR' + filetype):
+            rename(filename, '3_' + filename.split('_')[0] + filetype)
+
+
+def move_files():
     chdir('../data/')
     for filename in listdir('.'):
         
-        if filename.endswith('UL.jpg'):
-            rename(filename, '0_' + filename.split('_')[0] + '.jpg')
-
-        elif filename.endswith('LL.jpg'):
-            rename(filename, '1_' + filename.split('_')[0] + '.jpg')
-
-        elif filename.endswith('UR.jpg'):
-            rename(filename, '2_' + filename.split('_')[0] + '.jpg')
-
-        elif filename.endswith('LR.jpg'):
-            rename(filename, '3_' + filename.split('_')[0] + '.jpg')
-    chdir('../bin/')
-
+        if filename.startswith('0') or filename.startswith('1'):
+            move('./' + filename, './negatives/' + filename)
+        else:
+            move('./' + filename, './positives/' + filename)
+            
 
 if __name__ == '__main__':
-    rename_files()
+    rename_files('resize_frontal_faces', '.png')
+    move_files()
