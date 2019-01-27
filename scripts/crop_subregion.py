052dd6d8bcdac33e1c1be1a3862bab451915d97c
Jason Chang <jasonchang0@berkeley.edu>
2019-01-27 04:45:48 -0600

    fix corner mislabeling

--- scripts/crop_subregion.py
+++ scripts/crop_subregion.py
@@ -10,12 +10,12 @@ for file in glob.glob('*webp'):
         continue
 
     img = cv2.imread(file, cv2.IMREAD_COLOR)
-    w, h = img.shape[:2]
+    h, w = img.shape[:2]
 
-    UL = img[:w//2, :h//2]
-    UR = img[w//2:, :h//2]
-    LL = img[:w//2, h//2:]
-    LR = img[w//2:, h//2:]
+    UL = img[:h//2, :w//2]
+    UR = img[:h//2, w//2:]
+    LL = img[h//2:, :w//2]
+    LR = img[h//2:, w//2:]
 
     cv2.imwrite('../ROI/img{}_UL.png'.format(count), UL)
     cv2.imwrite('../ROI/img{}_UR.png'.format(count), UR)
